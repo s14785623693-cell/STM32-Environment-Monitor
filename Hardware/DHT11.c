ab5e184d6591df9195729ab37dc54804163e894a
WangZhiWei <s14785623693@gmail.com>
2026-09-12 21:57:35 +0800

    项目初始化

---
+++ Hardware/DHT11.c
@@ -0,0 +1,13 @@
+void DHT11_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_SetBits(GPIOA, GPIO_Pin_0);
+}
