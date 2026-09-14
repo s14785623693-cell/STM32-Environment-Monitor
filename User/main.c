8676bf318e45d6d6b246218e5d40ce32d8c76e55
WangZhiWei <s14785623693@gmail.com>
2026-09-14 22:42:43 +0800

    完成环境监测系统基础功能

--- User/main.c
+++ User/main.c
@@ -1,41 +1,55 @@
 #include "stm32f10x.h"
 #include "Delay.h"
 #include "OLED.h"
+#include "DHT11.h"
 #include "HCSR04.h"
 
-
+uint8_t Humidity;
+uint8_t Temperature;
 float Distance;
-
 
 int main(void)
 {
     OLED_Init();
-
+    DHT11_Init();
     HCSR04_Init();
 
-
-    OLED_ShowString(1, 1, "HC-SR04 TEST");
-
+    OLED_ShowString(1, 1, "Environment");
 
     while(1)
     {
-        Distance = HCSR04_GetDistance();
-
-
-        OLED_ShowString(2, 1, "Distance:");
-
-
-        if(Distance == 0)
+        // DHT11
+        if(DHT11_ReadData(&Humidity, &Temperature) == 0)
         {
-            OLED_ShowString(3, 1, "ERROR");
+            OLED_ShowString(2, 1, "Temp:");
+            OLED_ShowNum(2, 6, Temperature, 2);
+            OLED_ShowString(2, 8, "C");
+
+            OLED_ShowString(3, 1, "Humi:");
+            OLED_ShowNum(3, 6, Humidity, 2);
+            OLED_ShowString(3, 8, "%");
         }
         else
         {
-            OLED_ShowNum(3, 1, (uint16_t)Distance, 3);
-            OLED_ShowString(3, 5, "cm");
+            OLED_ShowString(2, 1, "Temp:ERROR");
+            OLED_ShowString(3, 1, "Humi:ERROR");
         }
 
+        // HC-SR04
+        Distance = HCSR04_GetDistance();
 
-        Delay_ms(200);
+        OLED_ShowString(4, 1, "Dist:");
+
+        if(Distance == 0)
+        {
+            OLED_ShowString(4, 6, "ERROR");
+        }
+        else
+        {
+            OLED_ShowNum(4, 6, (uint16_t)Distance, 3);
+            OLED_ShowString(4, 9, "cm");
+        }
+
+        Delay_ms(1000);
     }
 }
